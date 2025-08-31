a26cf526f544aac4503446514e40b8c0f01121c8
nk23852 <167011572+nk23852@users.noreply.github.com>
2025-08-31 16:28:32 +0530

    change mongo comment

--- Crowd/Backend/server.js
+++ Crowd/Backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//mongo connectiong
+//mongo connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
